a2e1b661f50eede313ff23b6dd227a2ebdc765f5
oosunla1 <oosunla1@my.westga.edu>
2025-09-04 14:43:19 -0400

    Implement addItem method and connect UI
    Implemented addItem to read input, create BillItem, add to Bill
    Generated and displayed bill text using BillView

--- Code/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
+++ Code/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
@@ -4,6 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import edu.westga.cs1302.lab3.model.Bill;
+import edu.westga.cs1302.lab3.model.BillItem;
 
 /**
  * Controller class for drawing various things to our canvas window.
@@ -21,11 +23,20 @@ public class MainWindow {
 
     @FXML
     private TextArea output;
-
+    
     @FXML
     void addItem(ActionEvent event) {
-    	String input = this.name.getText();
     	
+        String itemName = this.name.getText();
+        double itemAmount = Double.parseDouble(this.amount.getText());
+
+        Bill bill = new Bill();
+        BillItem item = new BillItem(itemName, itemAmount);
+        bill.addItem(item);
+
+        BillView view = new BillView();
+        String billText = view.getText(bill);
+        this.output.setText(billText);
     }
 
     /**
