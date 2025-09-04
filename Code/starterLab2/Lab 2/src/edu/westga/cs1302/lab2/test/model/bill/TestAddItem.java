79ed3422c51f674f0080c30f1e8dc84644b5dee3
oosunla1 <oosunla1@my.westga.edu>
2025-09-04 06:46:36 -0400

    Added test cases for Bill's addItem method. Tests run.

--- Code/starterLab2/Lab 2/src/edu/westga/cs1302/lab2/test/model/bill/TestAddItem.java
+++ Code/starterLab2/Lab 2/src/edu/westga/cs1302/lab2/test/model/bill/TestAddItem.java
@@ -1,5 +1,8 @@
 package edu.westga.cs1302.lab2.test.model.bill;
 
+import edu.westga.cs1302.lab2.model.Bill;
+import edu.westga.cs1302.lab2.model.BillItem;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -7,7 +10,35 @@ import org.junit.jupiter.api.Test;
 class TestAddItem {
 	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testWhenItemIsNull() {
+		Bill bill = new Bill();
+		assertThrows(
+				IllegalArgumentException.class,
+				() -> bill.addItem(null)
+		);
+	}
+	
+	@Test
+	public void testWhenItemAdded() {
+	    Bill bill = new Bill();
+	    BillItem item = new BillItem("Chips", 2.75);
+	    bill.addItem(item);
+	    var items = bill.getItems();
+	    assertEquals(1, items.size(), "Bill should contain one item");
+	    assertEquals(item, bill.getItems().get(0), "First item (Chips) should be the one added");
+	    
+	}
+	
+	@Test
+	public void testWhenMultipleItemsAdded() {
+		Bill bill = new Bill();
+	    BillItem item1 = new BillItem("Chips", 2.75);
+	    BillItem item2 = new BillItem("Bacon", 6.50);
+	    bill.addItem(item1);
+	    bill.addItem(item2);
+	    var items = bill.getItems();
+	    assertEquals(2, items.size(), "Bill should contain one item");
+	    assertEquals(item1, bill.getItems().get(0), "First item (Chips) should be the one added");
+	    assertEquals(item2, bill.getItems().get(1), "Second item (Bacon) should be the one added");
 	}
 }
