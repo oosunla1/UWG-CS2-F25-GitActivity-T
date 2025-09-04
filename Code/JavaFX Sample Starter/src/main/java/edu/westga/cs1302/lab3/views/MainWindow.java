d507fa557f50a10b23a4f1d248f94a3f591af43b
oosunla1 <oosunla1@my.westga.edu>
2025-09-04 13:08:31 -0400

    Designed a GUI
    Implemented Labels, Text Fields, a Text Area, and a Button into the GUI.

--- Code/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
+++ Code/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
@@ -1,5 +1,9 @@
 package edu.westga.cs1302.lab3.views;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  * Controller class for drawing various things to our canvas window.
@@ -8,7 +12,22 @@ package edu.westga.cs1302.lab3.views;
  * @version Fall 2025
  */
 public class MainWindow {
-    
+
+    @FXML
+    private TextField amount;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    private TextArea output;
+
+    @FXML
+    void addItem(ActionEvent event) {
+    	String input = this.name.getText();
+    	
+    }
+
     /**
      * Perform any needed initialization of UI components and underlying objects.
      */
