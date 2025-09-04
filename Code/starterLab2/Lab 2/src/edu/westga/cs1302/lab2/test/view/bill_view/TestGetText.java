de2d719336e9eea0c15b701233cf0ca031275d53
oosunla1 <oosunla1@my.westga.edu>
2025-09-04 08:43:21 -0400

    Added test cases for BillView's getText method. Tests run.

--- Code/starterLab2/Lab 2/src/edu/westga/cs1302/lab2/test/view/bill_view/TestGetText.java
+++ Code/starterLab2/Lab 2/src/edu/westga/cs1302/lab2/test/view/bill_view/TestGetText.java
@@ -1,5 +1,9 @@
 package edu.westga.cs1302.lab2.test.view.bill_view;
 
+import edu.westga.cs1302.lab2.model.Bill;
+import edu.westga.cs1302.lab2.model.BillItem;
+import edu.westga.cs1302.lab2.view.BillView;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -7,8 +11,65 @@ import org.junit.jupiter.api.Test;
 class TestGetText {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testWhenNoItemsInBill() {
+		Bill bill = new Bill();
+        BillView view = new BillView();
+
+        String actual = "ITEMS" + System.lineSeparator() + System.lineSeparator()
+                        + "SUBTOTAL - $0.0" + System.lineSeparator()
+                        + "TAX - $0.0" + System.lineSeparator()
+                        + "TIP - $0.0" + System.lineSeparator()
+                        + "TOTAL - $0.0";
+
+        assertEquals(actual, view.getText(bill), "Bill should be $0.0");
+	}
+	
+	@Test
+	public void testWhenOneItemInBill() {
+		Bill bill = new Bill();
+        bill.addItem(new BillItem("Sandwich", 12.50));
+
+        BillView view = new BillView();
+
+        double subTotal = 12.50;
+        double tax = subTotal * Bill.TAX_RATE;
+        double tip = subTotal * Bill.TIP_RATE;
+        double total = subTotal + tax + tip;
+
+        String actual = "ITEMS" + System.lineSeparator() +
+                          "Sandwich - 12.5" + System.lineSeparator() +
+                          System.lineSeparator() +
+                          "SUBTOTAL - $12.5" + System.lineSeparator() +
+                          "TAX - $" + tax + System.lineSeparator() +
+                          "TIP - $" + tip + System.lineSeparator() +
+                          "TOTAL - $" + total;
+
+        assertEquals(actual, view.getText(bill), "Checks the one item on the bill");
+	}
+	
+	@Test
+	public void testWhenMultipleItemsInBill() {
+		Bill bill = new Bill();
+        bill.addItem(new BillItem("Sandwich", 12.50));
+        bill.addItem(new BillItem("Soda", 2.50));
+
+        BillView view = new BillView();
+
+        double subTotal = 12.50 + 2.50;
+        double tax = subTotal * Bill.TAX_RATE;
+        double tip = subTotal * Bill.TIP_RATE;
+        double total = subTotal + tax + tip;
+
+        String actual = "ITEMS" + System.lineSeparator() +
+                          "Sandwich - 12.5" + System.lineSeparator() +
+                          "Soda - 2.5" + System.lineSeparator() +
+                          System.lineSeparator() +
+                          "SUBTOTAL - $15.0" + System.lineSeparator() +
+                          "TAX - $" + tax + System.lineSeparator() +
+                          "TIP - $" + tip + System.lineSeparator() +
+                          "TOTAL - $" + total;
+
+        assertEquals(actual, view.getText(bill), "Checks and diplays the items on the bill");
 	}
 
 }
