96a932fd28906e0bd37f8c6779f708f31084945b
oosunla1 <oosunla1@my.westga.edu>
2025-12-08 19:04:41 -0500

    Binding View and ViewModel for Storing Collections
    Implemented the appropriate databinding and event handlers for displaying, adding, and removing collections.

--- Code/project3Starter/Project 3/src/main/java/edu/westga/cs1302/task_tracker/views/MainWindow.java
+++ Code/project3Starter/Project 3/src/main/java/edu/westga/cs1302/task_tracker/views/MainWindow.java
@@ -1,5 +1,8 @@
 package edu.westga.cs1302.task_tracker.views;
 
+import edu.westga.cs1302.task_tracker.model.Collection;
+import edu.westga.cs1302.task_tracker.viewmodel.MainWindowViewModel;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,7 +10,6 @@ import javafx.scene.control.ContextMenu;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 
 /** Controller class for MainWindow of the Collection system.
  * 
@@ -15,11 +17,13 @@ import javafx.scene.input.MouseEvent;
  * @version Fall 2025
  */
 public class MainWindow {
-	@FXML private Button addCollectionButtom;
-    @FXML private ListView<String> collections;
+	@FXML private Button addCollection;
+    @FXML private ListView<Collection> collections;
     @FXML private TextField name;
     @FXML private Button removeCollectionButton;
     @FXML private MenuItem removeCollectionMenu;
+    
+    private MainWindowViewModel vm;
 
     /**
      * Adds a new collection to the ListView using the text entered
@@ -33,12 +37,7 @@ public class MainWindow {
      */
     @FXML
     void addCollection(ActionEvent event) {
-        String name = this.name.getText();
-
-        if (name != null && !name.isEmpty()) {
-            this.collections.getItems().add(name);
-            this.name.clear();
-        }
+    	this.vm.addCollection();
     }
 
     /**
@@ -51,24 +50,7 @@ public class MainWindow {
      */
     @FXML
     void removeCollection(ActionEvent event) {
-        String selected = this.collections.getSelectionModel().getSelectedItem();
-        if (selected != null) {
-            this.collections.getItems().remove(selected);
-        }
-    }
-
-    /**
-     * Handles updating UI state based on which collection
-     * the user selects from the ListView.
-     *
-     * @precondition none
-     * @postcondition The selected collection is stored in the selection model.
-     *
-     * @param event required by JavaFX but not used
-     */
-    @FXML
-    void selectCollection(MouseEvent event) {
-        //String selected = this.collections.getSelectionModel().getSelectedItem();
+    	this.vm.removeSelectedCollection();
     }
 
     /** Perform any needed initialization of UI components and underlying objects.
@@ -79,16 +61,16 @@ public class MainWindow {
      */
     @FXML
     public void initialize() {
+    	this.vm = new MainWindowViewModel();
+    	this.name.textProperty().bindBidirectional(this.vm.newCollection());
+    	this.collections.setItems(this.vm.getCollections());
+    	this.vm.selectedCollection().bind(this.collections.getSelectionModel().selectedItemProperty());
+    	
     	ContextMenu menu = new ContextMenu();
         MenuItem removeItem = new MenuItem("Remove Collection");
         menu.getItems().add(removeItem);
         this.collections.setContextMenu(menu);
         
-        removeItem.setOnAction(event -> {
-            String selected = this.collections.getSelectionModel().getSelectedItem();
-            if (selected != null) {
-                this.collections.getItems().remove(selected);
-            }
-        });
+        removeItem.setOnAction(event -> this.vm.removeSelectedCollection());
     }
 }
